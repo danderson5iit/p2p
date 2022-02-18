089c25c141637e922dd0c8d046ce7066b7f985c4
morganSuch <63384862+morganSuch@users.noreply.github.com>
2022-02-18 10:10:56 -0800

    Delete IndexInterface.java

--- p2p/src/IndexInterface.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-import java.util.ArrayList;
-
-public interface IndexInterface extends Remote{
-
- public void registerFiles(String peerId, String filename,String portno,String srcDir)throws RemoteException; 
-  public ArrayList<FileDetails> search(String filename)throws RemoteException; 
-}
