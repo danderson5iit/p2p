5543ddc3b33d4ea9efbba47d3cae093786093ad2
David Anderson <79073069+danderson5iit@users.noreply.github.com>
2022-02-19 13:20:37 -0600

    Add files via upload

---
+++ IndexInterface.java
@@ -0,0 +1,10 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+public interface IndexInterface extends Remote{
+
+ public void registerFiles(String peerId, String filename,String portno,String srcDir)throws RemoteException; 
+ public void deregisterFiles(String peerId, String fileName,String portno,String srcDir) throws RemoteException;
+ public ArrayList<FileDetails> search(String filename)throws RemoteException; 
+}
