773d5a34ec46a91a22599fcecb4ed5c65c939d1f
morganSuch <63384862+morganSuch@users.noreply.github.com>
2022-02-20 06:00:05 -0800

    Add files via upload

---
+++ sunday/FileConstructor.java
@@ -0,0 +1,10 @@
+import java.io.Serializable;
+
+public class FileConstructor implements Serializable
+{
+        String peerId;
+	String FileName;
+        String portNumber;
+        String SrcDirectory;
+}
+
